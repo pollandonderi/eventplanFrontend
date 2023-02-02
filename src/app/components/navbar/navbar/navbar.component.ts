b177e23b395ba44cabe5672f441ed3b320446416
Polland Onderi <pollandsabisa@gmail.com>
2023-02-02 07:51:12 +0300

    add navbar, with errors, to be corrected

--- src/app/components/navbar/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar/navbar.component.ts
@@ -7,4 +7,22 @@ import { Component } from '@angular/core';
 })
 export class NavbarComponent {
 
+const primaryNav = document.querySelector('.primary-navigation');
+const navMobile = document.querySelector('.mobile-nav-toggle');
+
+navMobile.addEventListener('click', () =>{
+  const visibility = primaryNav.getAttribute("data-visible");
+  console.log(visibility)
+
+  if (visibility === "false"){
+    primaryNav.setAttribute("data-visible", true);
+    navMobile.setAttribute("aria-expanded", true);
+  }
+  else if (visibility === true){
+    primaryNav.setAttribute("data-visible" , false);
+    navMobile.setAttribute("aria-expanded", false);
+  }
+})
+
+
 }
