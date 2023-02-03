761a588811e101d017b4fde1ba413feab826a873
Polland Onderi <pollandsabisa@gmail.com>
2023-02-03 17:42:16 +0300

    doing some work on the landing made changes on cards and did some placement on the svgs

--- src/app/components/navbar/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar/navbar.component.ts
@@ -1,28 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
-export class NavbarComponent {
-
-const primaryNav = document.querySelector('.primary-navigation');
+export class NavbarComponent implements OnInit {
+  constructor(){}
+  ngOnInit(): void {
+    const primaryNav = document.querySelector('.primary-navigation');
 const navMobile = document.querySelector('.mobile-nav-toggle');
 
-navMobile.addEventListener('click', () =>{
-  const visibility = primaryNav.getAttribute("data-visible");
+navMobile?.addEventListener('click', () =>{
+  const visibility = primaryNav?.getAttribute("data-visible");
   console.log(visibility)
 
-  if (visibility === "false"){
-    primaryNav.setAttribute("data-visible", true);
-    navMobile.setAttribute("aria-expanded", true);
+  if (!visibility ||(visibility === "false")){
+    primaryNav?.setAttribute("data-visible", "true");
+    navMobile.setAttribute("aria-expanded", "true");
   }
-  else if (visibility === true){
-    primaryNav.setAttribute("data-visible" , false);
-    navMobile.setAttribute("aria-expanded", false);
+  else{
+    primaryNav?.setAttribute("data-visible" , "false");
+    navMobile.setAttribute("aria-expanded", "false");
   }
 })
+    
+  }
+
+
 
 
 }
