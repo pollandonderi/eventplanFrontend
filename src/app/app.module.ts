a8e94b58bd524a0302bcbed4816cb986af240ad2
polland <pollandsabisa@gmail.com>
2023-03-06 10:55:22 +0300

    correcting the styles by ensuring i have global variables in one file which is light

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PrimarybuttonComponent } from './components/primarybutton/primarybutton/primarybutton.component';
-import { SecondarybuttonComponent } from './components/secondarybutton/secondarybutton/secondarybutton.component';
 import { NavbarComponent } from './components/navbar/navbar/navbar.component';
 import { CardComponent } from './components/card/card.component';
 import { CardseventsComponent } from './components/cardsevents/cardsevents.component';
@@ -14,15 +11,10 @@ import { HomepageComponent } from './components/homepage/homepage.component';
 import { AboutusComponent } from './components/aboutus/aboutus.component';
 import { WhatexpectedComponent } from './whatexpected/whatexpected.component';
 import { GetinspiredComponent } from './getinspired/getinspired.component';
-import { LoginComponent } from './login/login.component';
-import { MerchandiseComponent } from './merchandise/merchandise.component';
-import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PrimarybuttonComponent,
-    SecondarybuttonComponent,
     NavbarComponent,
     CardComponent,
     CardseventsComponent,
